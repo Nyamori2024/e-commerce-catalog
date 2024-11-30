745205eec758e4f761b4825507924bd5fc41dd80
Christian Mathews Abuto Fullstack/Backend /Frontend Developer <abutochris2015@gmail.com>
2024-11-30 15:33:10 +0000

    updated components

--- src/redux/types.ts
+++ src/redux/types.ts
@@ -1,11 +1,8 @@
-
-  
-  export interface Product {
-    id: number;
-    title: string;
-    price: number;
-    image: string;
-    rating: { rate: number }; // Assuming each product has a rating object
-    thumbnail: string;
-  }
-  
+export interface Product {
+  id: number;
+  title: string;
+  price: number;
+  image: string;
+  rating: { rate: number }; // Assuming each product has a rating object
+  thumbnail: string;
+}
